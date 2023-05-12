f6ae4a5dd7ddd16ef609189a39f7d214efc3d223
David <agutierrezp1701@alumno.ipn.mx>
2023-05-11 18:43:19 -0600

    Ciclos para cambio de valores
    ciclos para cambiara 1 los valores

--- PracticaMineria.R
+++ PracticaMineria.R
@@ -1,36 +1,53 @@
-salarios <- read.csv("ds_salaries.csv")
+install.packages("stringr")
+library("stringr")
 options(scipen=999)
 plot(salarios[,"work_year"], salarios[,"salary_in_usd"], main = "Relación entre el año en el que se trabaja y el salario recibido",
      xlab = "Año", ylab = "Salario percibido", xlim=c(2019,2023), ylim=c(10000,400000))
 modelo <- lm(salarios[,"salary_in_usd"]~salarios[,"work_year"])
 summary(modelo)
 #Comparación de prediccion de trabajar en ciencia de datos en 2020 a 2023
-work_2020<-data.frame(salarios[salarios$work_year == '2020,5'])
+work_2020<-data.frame(salarios[salarios$work_year == '2020.0'])
 prediccion <-predict(modelo, newdata = work_2020)
 prediccion
 plot(salarios[,"work_year"], salarios[,"salary_in_usd"])
 abline(modelo)
 
-#####
-
-datos <- data.frame("salarios"=salarios[,"salary_in_usd"],"anios"=salarios[,"work_year"])
-plot(datos$anios,datos$salarios)
-datos$xy <- datos$anios*datos$salarios
-datos$x2 <- datos$anios*datos$anios
-str(datos)
-n=nrow(datos)
-sx <- sum(datos$anios)
-sy <- sum(datos$salarios)
-sxy <- sum(datos$xy)
-sx2 <- sum(datos$x2)
-m<-(n*sxy-sx*sy)/(n*sx2-sx*sx)
-b<-(sy*sx2-sx*sxy)/(n*sx2-sx*sx)
-plot(datos$x,datos$y,lwd=2, col="red")
-datos<-rbind(datos,0)
-y1<-b+datos$x*m
-lines(datos$x,y1,type="l",lwd=2,col="blue")
 
 
+##### as.factor()
+### relacion entre el nivel de experiencia, salario, tamaño de la empresa y el pais
+### 
 
+experiencia <- salarios$experience_level
+experiencia
+experienciaNueva <- list()
+
+tamano <- salarios$company_size
+tamano
+tamanoNuevo <- list()
+
+# Cambiar valores de exp a numeros
+for (x in 1 : length(experiencia)) {
+  if (str_detect(experiencia[[x]], "SE")) experienciaNueva[x] <- 1 #1 para SE
+   if(str_detect(experiencia[[x]],"MI")) experienciaNueva[x] <- 2 #2 para MI
+   if(str_detect(experiencia[[x]],"EN")) experienciaNueva[x] <- 3 #3 para EN
+   if(str_detect(experiencia[[x]],"EX")) experienciaNueva[x] <- 4 #4 para EX
+  
+}
+
+#Cambiar valores de tamaño de empresa a numeros
+for (x in 1 : length(tamano)) {
+  if (str_detect(tamano[[x]], "M")) tamanoNuevo[x] <- 2 #2 para mediano
+  if(str_detect(tamano[[x]],"L")) tamanoNuevo[x] <- 3 #3 para grande
+  if(str_detect(tamano[[x]],"S")) tamanoNuevo[x] <- 1 #1 para pequeño
+}
+
+paises <- list()
+
+
+
+
+tamanoNuevo
+experienciaNueva
 
 
